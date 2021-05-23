377a12bc1a848a467cd7f5b91d037ec2799bdbe4
Runtime-Ali <57050083+Runtime-Ali@users.noreply.github.com>
2021-05-22 20:44:59 -0400

    adjust database script

--- anubis/migrations/002.sql
+++ anubis/migrations/002.sql
@@ -1,7 +1,3 @@
-CREATE TABLE IF NOT EXISTS applied_migrations (
-    number INTEGER PRIMARY KEY
-);
-
 CREATE TABLE IF NOT EXISTS ignored_roles (
     guild_id    INTEGER     NOT NULL,
     role_id     INTEGER     NOT NULL,
